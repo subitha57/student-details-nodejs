a58084b0b0954ecf30637208ea8167fb8de6c17c
subitha57 <subitha.varatharajan@elonnativesystem.com>
2024-10-18 11:07:57 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-// app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const studentRoutes = require('./routes/students');
+
+const app = express();
+
+// Middleware to parse request bodies
+app.use(bodyParser.json());
+
+// MongoDB Atlas connection
+mongoose.connect('mongodb+srv://subitha:12345@cluster0.cje3d.mongodb.net/studentDB?retryWrites=true&w=majority')
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.error('Connection failed', err));
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB Atlas'));
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Student API');
+});
+
+// Student routes
+app.use('/students', studentRoutes);
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+
+/* app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -28,4 +62,4 @@ app.use('/students', studentRoutes);
 // Start the server
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-});
+});*/
